2b17d249e0749a24676a3d739fb52fbef83d507b
andrew8gmf <andrew8gmf@gmail.com>
2022-03-10 15:42:52 -0300

    feat: get the values of buttons and inputs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,9 +8,38 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  billValue: any;
+  customTipValue: any;
+  peopleValue: any;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onBill(event: any) {
+    this.billValue = event.target.value;
+    if (this.billValue) {
+      console.log(this.billValue)
+    }
+  }
+
+  onTip(value :any) {
+    console.log(value);
+  }
+
+  onCustomTip(event: any) {
+    this.customTipValue = event.target.value;
+    if (this.customTipValue) {
+      console.log(this.customTipValue)
+    }
+  }
+
+  onPeople(event: any) {
+    this.peopleValue = event.target.value;
+    if (this.peopleValue) {
+      console.log(this.peopleValue)
+    }
   }
 
 }
